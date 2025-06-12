4ca47d14dd6f1ff55964135861c3b302661ed4ce
Jiri Volf <ji.volf@post.cz>
2025-06-12 13:01:48 +0200

    rename

--- api/questions/getAllCategoryQuestions.ts
+++ api/questions/getAllCategoryQuestions.ts
@@ -24,7 +24,7 @@ export default async function handler(req, res) {
 
     return res.status(200).json({
       success: true,
-      categoryQuestions: questions,
+      questions: questions,
     });
   } catch (error) {
     return res.status(500).json({ success: false, message: error.message });
--- api/questions/getQuizQuestions.ts
+++ api/questions/getQuizQuestions.ts
@@ -38,7 +38,7 @@ export default async function handler(req, res) {
 
     return res.status(200).json({
       success: true,
-      quizQuestions: shuffled,
+      questions: shuffled,
     });
   } catch (error) {
     return res.status(500).json({ success: false, message: error.message });
